8bda893deb05bb68de2246ded78cbfea5cebdda2
unknown <tenshiamvs@gmail.com>
2024-08-31 23:41:46 +0200

    Defined needed variables for JS Script

--- rps.js
+++ rps.js
@@ -4,3 +4,16 @@
 // Return value as String
 // Get User Input (Rock, Paper or Scissor) make it case insensetive display undefined if user doesnt make a valid choice
 
+const playerChoices = document.getElementById("playerChoices");
+const lifeseDisplay = document.getElementById("lifeseDisplay");
+const playerLifes = document.getElementById("playerLifes");
+const computerLifes = document.getElementById("computerLifes");
+const buttonRock = document.getElementById("buttonRock");
+const buttonPaper = document.getElementById("buttonPaper");
+const buttonScissor = document.getElementById("buttonScissor");
+
+function getComputerChoice(){
+    
+
+
+}
