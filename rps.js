987694e91b16e47f58fc63061f90dfa30691de8a
unknown <tenshiamvs@gmail.com>
2024-09-02 20:48:46 +0200

    finished the game logic and changed UI

--- rps.js
+++ rps.js
@@ -22,10 +22,10 @@ let playerLifes = 5;
 let computerLifes = 5;
 function checkGameOver() {
     if (playerLifes === 0) {
-        console.log("You don't have any lives left. YOU LOST!");
+        document.getElementById("gameResult").innerHTML = "You don't have any lives left. YOU LOST!";
         return true;
     } else if (computerLifes === 0) {
-        console.log("YOU WON! The Computer has no lives left.");
+        document.getElementById("gameResult").innerHTML ="YOU WON! The Computer has no lives left.";
         return true;
     }
     return false;
@@ -41,36 +41,36 @@ function playGame(){
         let randomNum = Math.floor(Math.random() * 3) + 1;
         choiceValue = 1;
         if(choiceValue === randomNum){
-            console.log("Its a Tie")
+            document.getElementById("gameResult").innerHTML ="Its a Tie"
         }
     
         else if(choiceValue === 1 && randomNum === 2){
             playerLifes = playerLifes -= 1;
-            console.log(`You lost paper beats rock. Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`);
+            document.getElementById("gameResult").innerHTML =`You lost paper beats rock. Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`;
         }
     
         else if(choiceValue === 2 && randomNum === 3){
             playerLifes = playerLifes -= 1;
-            console.log(`You lost scissor beats paper Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`);
+            document.getElementById("gameResult").innerHTML =`You lost scissor beats paper Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`;
         }
     
         else if(choiceValue === 3 && randomNum === 1){
             playerLifes = playerLifes -= 1;
-            console.log(`You lost rock beats scissor Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`);
+            document.getElementById("gameResult").innerHTML =`You lost rock beats scissor Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`;
     
         }
     
         else if(choiceValue === 1 && randomNum === 3){
             computerLifes = computerLifes -= 1;
-            console.log(`You won rock beats scissor Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`);
+            document.getElementById("gameResult").innerHTML =`You won rock beats scissor Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`;
         }
         else if(choiceValue === 2 && randomNum === 1){
             computerLifes = computerLifes -= 1;
-            console.log(`You won paper beats rock Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`);
+            document.getElementById("gameResult").innerHTML =`You won paper beats rock Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`;
         }
         else if(choiceValue === 3 && randomNum === 2){
             computerLifes = computerLifes -= 1;
-            console.log(`You won scissor beats paper Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`)
+            document.getElementById("gameResult").innerHTML =`You won scissor beats paper Your lifes: ${playerLifes} Computer lifes: ${computerLifes}`
 
         }
        
